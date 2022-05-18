dcf875a499db989698f3e507be06c21a0bf5e403
BublIk2005 <mihailgorbunov@list.ru>
2022-05-18 17:19:20 +0300

    init

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"text/template"
+)
+
+var tpl = template.Must(template.ParseFiles("index.html"))
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tpl.Execute(w, nil)
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":"+port, mux)
+}
